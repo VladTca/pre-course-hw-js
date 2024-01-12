7939118bcb15339668c7c645c6d9f5e4168b9c31
VladTca <votsats@gmail.com>
2024-01-12 18:49:52 +0100

    les 2 strt fix

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,5 +1,5 @@
-let number1 = Math.floor(Math.random() * 100)
-console.log (number1++);
-console.log (++number1);
-console.log (number1--);
-console.log (--number1);
+let number = 1;
+console.log (number++);
+console.log (++number);
+console.log (number--);
+console.log (--number);
