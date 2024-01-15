92588f58403c1b9bb8e9d60ef6eb771b2d7032e0
VladTca <votsats@gmail.com>
2024-01-15 19:41:23 +0100

    les3-1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,12 @@
 // code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+  return colors
+    .filter(
+      (word) => word === "черный" || word === "красный" || word === "желтый",
+    )
+    .join("-");
+}
+
+console.log(createColorString());
